8de7695ab63ec5042beed5d5cc315ea3e497b077
EugeniusMiroshnichenko <zhnya.miroshnichenko@mail.ru>
2025-12-03 12:01:38 +0500

    Swap pointer done

--- 02_week/tasks/swap_ptr/swap_ptr.cpp
+++ 02_week/tasks/swap_ptr/swap_ptr.cpp
@@ -1,7 +1,9 @@
 #include <stdexcept>
 
-
-void SwapPtr(int* ptr1, int* ptr2) {
-    int sum = *ptr1 + *ptr2;
-    throw std::runtime_error{"Not implemented"};
+template<typename T>
+// Сделаем через generic, главное чтобы приходил какой-то указатель
+void SwapPtr(T*& ptr1, T*& ptr2) {
+    T* temp = ptr1;
+    ptr1 = ptr2;
+    ptr2 = temp;
 }
