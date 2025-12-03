cec01a6915341ee2d5e500d7e67b4da9a7cbd7d5
EugeniusMiroshnichenko <zhnya.miroshnichenko@mail.ru>
2025-12-03 09:52:02 +0500

    Func array done

--- 02_week/tasks/swap_ptr/swap_ptr.cpp
+++ 02_week/tasks/swap_ptr/swap_ptr.cpp
@@ -1,6 +1,7 @@
 #include <stdexcept>
 
 
-void SwapPtr(/* write arguments here */) {
+void SwapPtr(int* ptr1, int* ptr2) {
+    int sum = *ptr1 + *ptr2;
     throw std::runtime_error{"Not implemented"};
 }
