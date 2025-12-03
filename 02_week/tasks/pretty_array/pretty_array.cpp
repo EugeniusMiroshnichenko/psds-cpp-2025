f13237ba6e915c18993ce8dc7690ae70f25f9705
EugeniusMiroshnichenko <zhnya.miroshnichenko@mail.ru>
2025-12-03 11:47:27 +0500

    Pretty array done

--- 02_week/tasks/pretty_array/pretty_array.cpp
+++ 02_week/tasks/pretty_array/pretty_array.cpp
@@ -1,6 +1,27 @@
 #include <stdexcept>
 
 
-void PrintArray(/* write arguments here */) {
-    throw std::runtime_error{"Not implemented"};
+void PrintArray(const int* start, const int* end, size_t limit = 0) {
+    std::string result = "[";
+    size_t counter = 0;
+    for (; start!=end; ++start){
+        ++counter;
+        result += std::to_string(*start);
+        if (end - start - 1 == 0){
+            // Последний элемент
+            continue;
+        }
+        else if ((limit != 0) && counter == limit)
+        {
+            // Переход на новую строку
+            counter = 0;
+            result += ", ...\n ";
+        }
+        else {
+            // Просто некоторый элемент
+            result += ", ";
+        }
+    }
+    result += "]\n";
+    std::cout << result;
 }
